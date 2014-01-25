cee7bf91f597ce0dd2102177e3759705b4271f28
Dave Jeffery <dave@davejeffery.com>
2014-01-25 17:09:36 +0000

    Add simple tests

---
+++ test/config/testacular.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '../../',
+    frameworks: ['jasmine'],
+    files: [
+  	  'examples/todomvc/bower_components/angular/angular.js',
+		  'examples/todomvc/bower_components/angular-classy/angular-classy.js',
+		  'examples/todomvc/bower_components/angular-mocks/angular-mocks.js',
+		  'examples/todomvc/js/app.js',
+		  'examples/todomvc/js/*/*.js',
+		  'test/unit/*.js'
+    ],
+    autoWatch: true,
+    browsers: ['Chrome']
+  });
+};
+
---
+++ test/readme.md
@@ -0,0 +1,10 @@
+Angular Classy Unit Tests
+=========================
+
+At the moment we are just testing the controller on the TodoMVC example app.
+This gives almost full test coverage (the exceptiong is watch collections) but I'll add them soon.
+
+To run the test suite, run these commands:
+
+    npm install
+    npm test
