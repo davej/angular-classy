53d59a3a1d0492a0e3402532c3a1d81c7af078b7
Dave Jeffery <dave@davejeffery.com>
2014-04-03 18:37:39 +0100

    module.classy namespace and options object for customising controllers (#4)
    module.cC = module.classyController = module.classy.controller
    Defaults object and options override at the module and controller
    instance level
    Reorder source to be more readable
    Todo example using controllerAs

--- examples/todomvc/js/controllers/todoCtrl-asController.js
+++ examples/todomvc/js/controllers/todoCtrl-asController.js
@@ -7,43 +7,46 @@
  * - exposes the model to the template and provides event handlers
  */
 
+todomvc.classy.options.controller = {
+	addFnsToScope: false
+}
+
 todomvc.classy.controller({
 	name: 'TodoCtrl',
 	inject: ['$scope', '$location', 'todoStorage', 'filterFilter'],
-
+	
 	init: function() {
-		this.todos = this.$scope.todos = this.todoStorage.get();
+		this.todos = this.todoStorage.get();
 
-		this.$scope.newTodo = '';
-		this.$scope.editedTodo = null;
+		this.newTodo = '';
+		this.editedTodo = null;
 
 		if (this.$location.path() === '') {
 			this.$location.path('/');
 		}
-		this.$scope.location = this.$location;
 	},
 
 	watch: {
-		'location.path()': function(path) {
-			this.$scope.statusFilter = (path === '/active') ?
+		'todoCtrl.$location.path()': function(path) {
+			this.statusFilter = (path === '/active') ?
 				{ completed: false } : (path === '/completed') ?
 				{ completed: true } : null;
 		},
-		'{object}todos': '_onTodoChange'
+		'{object}todoCtrl.todos': '_onTodoChange'
 	},
 
 
 	_onTodoChange: function (newValue, oldValue) {
-		this.$scope.remainingCount = this.filterFilter(this.todos, { completed: false }).length;
-		this.$scope.completedCount = this.todos.length - this.$scope.remainingCount;
-		this.$scope.allChecked = !this.$scope.remainingCount;
+		this.remainingCount = this.filterFilter(this.todos, { completed: false }).length;
+		this.completedCount = this.todos.length - this.remainingCount;
+		this.allChecked = !this.remainingCount;
 		if (newValue !== oldValue) { // This prevents unneeded calls to the local storage
 			this.todoStorage.put(this.todos);
 		}
 	},
 
 	addTodo: function () {
-		var newTodo = this.$scope.newTodo.trim();
+		var newTodo = this.newTodo.trim();
 		if (!newTodo.length) {
 			return;
 		}
@@ -53,27 +56,27 @@ todomvc.classy.controller({
 			completed: false
 		});
 
-		this.$scope.newTodo = '';
+		this.newTodo = '';
 	},
 
 	editTodo: function (todo) {
-		this.$scope.editedTodo = todo;
+		this.editedTodo = todo;
 		// Clone the original todo to restore it on demand.
-		this.$scope.originalTodo = angular.extend({}, todo);
+		this.originalTodo = angular.extend({}, todo);
 	},
 
 	doneEditing: function (todo) {
-		this.$scope.editedTodo = null;
+		this.editedTodo = null;
 		todo.title = todo.title.trim();
 
 		if (!todo.title) {
-			this.$scope.removeTodo(todo);
+			this.removeTodo(todo);
 		}
 	},
 
 	revertEditing: function (todo) {
-		this.todos[this.todos.indexOf(todo)] = this.$scope.originalTodo;
-		this.$scope.doneEditing(this.$scope.originalTodo);
+		this.todos[this.todos.indexOf(todo)] = this.originalTodo;
+		this.doneEditing(this.originalTodo);
 	},
 
 	removeTodo: function (todo) {
@@ -81,7 +84,7 @@ todomvc.classy.controller({
 	},
 
 	clearCompletedTodos: function () {
-		this.$scope.todos = this.todos = this.todos.filter(function (val) {
+		this.todos = this.todos.filter(function (val) {
 			return !val.completed;
 		});
 	},
